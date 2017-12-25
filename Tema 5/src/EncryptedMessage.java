efb8ae320ab68c8242b6b29a301e79a076254556
javi2gar <javi2gar@gmail.com>
2017-12-25 19:28:49 +0000

    Ejercicio 1 (RSA). Código que genera unas claves pública y privada RSA de 2048 bits que usa para encriptar y desencriptar un mensaje

---
+++ Tema 5/src/EncryptedMessage.java
@@ -0,0 +1,56 @@
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+
+public class EncryptedMessage {
+
+    public static void main(String [] args) throws Exception {
+
+        String message = "This is a secret message";
+
+        // generate public and private keys
+        KeyPair keyPair = buildKeyPair();
+        PublicKey pubKey = keyPair.getPublic();
+        PrivateKey privateKey = keyPair.getPrivate();
+
+        // original message
+        System.out.print("Mensaje original: ");
+        System.out.println(message);  // <<encrypted message>>
+
+        // encrypt the message
+        byte [] encrypted = encrypt(privateKey, message);
+        System.out.print("Mensaje encriptado: ");
+        System.out.println(new String(encrypted));  // <<encrypted message>>
+
+        // decrypt the message
+        byte[] secret = decrypt(pubKey, encrypted);
+        System.out.print("Mensaje desencriptado: ");
+        System.out.println(new String(secret));     // This is a secret message
+
+    }
+
+    public static KeyPair buildKeyPair() throws NoSuchAlgorithmException {
+        final int keySize = 2048;
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(keySize);
+        return keyPairGenerator.genKeyPair();
+    }
+
+    public static byte[] encrypt(PrivateKey privateKey, String message) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+
+        return cipher.doFinal(message.getBytes());
+    }
+
+    public static byte[] decrypt(PublicKey publicKey, byte [] encrypted) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.DECRYPT_MODE, publicKey);
+
+        return cipher.doFinal(encrypted);
+    }
+}
